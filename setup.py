1cf0e2329911ba388ad98f8317d38e1c7ff23521
xxXER0 <kuldeepchamuah830@gmail.com>
2023-07-26 21:27:38 +0530

    requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "chicken-disease-classification-project"
+AUTHOR_USER_NAME = "xxXER0"
+SRD_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "kuldeepchamuah830@gmail.com"
